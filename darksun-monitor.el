ac8979103ad1477d1708909121498ba2e285de82
lujw <lujw@drcbank.com>
2015-02-13 21:28:03 +0800

    绠€鍖?閲嶆帓

--- darksun-monitor.el
+++ darksun-monitor.el
@@ -1,5 +1,20 @@
 (require 'notifications)
 
+(defstruct monitor exam-cmd
+		   reaction-rules)
+
+(defgroup monitor-group nil
+  "远程监控monitor")
+
+(defcustom *process-current-monitor-map* (make-hash-table)
+  "存放每个process当成正在处理哪个monitor")
+
+(defcustom *process-monitors-map* (make-hash-table)
+  "存放每个process对应的monitor")
+
+(defcustom *time-out* 10
+  "每个monitor执行后获取output的超时时间")
+
 (defun dbus-avaliable-p ()
 	"判断Emacs是否编译时支持D-Bus"
 	(featurep 'dbusbind))
@@ -23,73 +38,55 @@
 		(process (or process (get-buffer-process (current-buffer)))))
 	(process-send-string process command)))
 
-(defun make-output-handler (reaction-rules)
-  "创建filter-function
-
-该filter-function根据handler-rules的规则来匹配后续动作.
+(defun monitor-filter-function (process output)
+  "该filter-function根据handler-rules的规则来匹配后续动作.
 
 handler-rules的格式为由(match . action)组成的alist
 
 当process的output匹配matchN时,执行actionN命令:若action为字符串,则往process发送action命令,否则action为函数,它接收output作为参数,并返回要发送給process的命令字符串"
-  (lexical-let ((rules reaction-rules))
-	(lambda (process output)
-	  (internal-default-process-filter process output)
-	  (let* ((rule (assoc-if (lambda (match)
-							   (or (eq match t)
-								   (string-match-p match output)))
-							 rules))
-			 (action (cdr rule)))
-		(when rule
-		  ;; 若dbus可用,则使用notification通知
-		  (when (dbus-avaliable-p)
-			(notifications-notify :title (process-name process)
-								  :body output))
-		  ;; 执行action动作
-		  (cond ((stringp action)
-				 (execute-monitor-command action process))
-				((functionp action)
-				 (execute-monitor-command (funcall action  output) process))
-				(t (message "error action type[%s]" (type-of action)))))))))
-
-(defun do-monitor-1 (process time-out command reaction-rules )
-  "向process发起监控命令,并根据handler-rules来根据输出执行相应的action"
-  (let* ((time-out (or time-out 2))
-		 (process (or process (get-buffer-process (current-buffer)))))
-	(set-process-filter process (make-output-handler reaction-rules))
-	(execute-monitor-command command process)
-	(accept-process-output process time-out nil t)))
-
-(defstruct monitor exam-cmd
-		   reaction-rules)
-
-(defun do-monitor (process time-out monitor  )
-  "向process发起监控命令,并根据reaction-rules来根据输出执行相应的action"
-  (let ((exam-cmd (monitor-exam-cmd monitor))
-		(reaction-rules (monitor-reaction-rules monitor)))
-	(do-monitor-1 process time-out exam-cmd reaction-rules)))
-
-(defun do-monitors (process time-out &rest monitors)
-  (dolist (monitor monitors)
-	(do-monitor process time-out monitor)))
-
-(defgroup monitor nil
-  "远程监控monitor")
-(defcustom *process-monitors-map* (make-hash-table)
-  "存放每个process对应的monitor")
-(defcustom *time-out* 10
-  "每个monitor执行后获取output的超时时间")
+  (internal-default-process-filter process output)
+  (let* ((reaction-rules (monitor-reaction-rules (gethash process *process-current-monitor-map*)))
+		 (rule (assoc-if (lambda (match)
+						   (or (eq match t)
+							   (string-match-p match output)))
+						 reaction-rules))
+		 (action (cdr rule)))
+	(when rule
+	  ;; 若dbus可用,则使用notification通知
+	  (when (dbus-avaliable-p)
+		(notifications-notify :title (process-name process)
+							  :body output))
+	  ;; 执行action动作
+	  (cond ((stringp action)
+			 (execute-monitor-command action process))
+			((functionp action)
+			 (execute-monitor-command (funcall action  output) process))
+			(t (message "error action type[%s]" (type-of action)))))))
 
 (defun start-monitor-process (remote usr pwd)
   "创建一个process用于执行monitor
 
 当该process没有存放于`*process-monitors-map*'中作为key时,则加入到`*process-monitors-map*'中
 
-该函数返回连接到usr@remote的process"
+该函数返回连接到usr@remote的process,并且其filter-function为`monitor-filter-functiion'"
   (let (process)
 	(setq process (make-or-raise-connect-by-plink remote usr pwd))
 	(unless (gethash process *process-monitors-map*)
 	  (setf (gethash process *process-monitors-map*) nil))
+	(set-process-filter process #'monitor-filter-function)
 	process))
+
+(defun do-monitor (process time-out monitor  )
+  "向process发起监控命令,并根据reaction-rules来根据输出执行相应的action"
+  (let ((exam-cmd (monitor-exam-cmd monitor))
+		(reaction-rules (monitor-reaction-rules monitor)))
+	(setf (gethash process *process-current-monitor-map*) monitor)
+	(execute-monitor-command exam-cmd process)
+	(accept-process-output process time-out nil t)))
+
+(defun do-monitors (process time-out &rest monitors)
+  (dolist (monitor monitors)
+	(do-monitor process time-out monitor)))
 
 (defun add-process-monitor (process monitor)
   "为process增加monitor"
