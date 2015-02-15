60e195f4e095ee139ba5a30c73be45d9f4ca778c
lujun9972 <lujun9972@gmail.com>
2015-02-15 22:21:47 +0800

    增加宏with-filter-function
    增加宏with-filter-function,用于为process临时设置新的filter function
    使用宏with-filter-function重构函数get-process-complete-output

--- darksun-process-helper.el
+++ darksun-process-helper.el
@@ -20,69 +20,77 @@
 		(process-put process 'output (concat last-output output))
 		(funcall filter-function process output)))))
 
+(defmacro with-filter-function (process filter-function &rest bodys)
+  "临时将process的filter functiion设置为新的filter-function"
+  (let* ((ori-filter-function (gensym)))
+	`(let ((,ori-filter-function (process-filter ,process)))
+	   (set-process-filter ,process ,filter-function)
+	   ,@bodys
+	   (set-process-filter ,process ,ori-filter-function))))
+
 (defun get-process-complete-output (process end-regex-or-time)
   "获取process的完整output"
-  (let ((originally-filter-function (process-filter process))
-		output)
-	(set-process-filter process (make-output-filter-function originally-filter-function))
-	(cond ((stringp end-regex-or-time)
-		   (while (not (string-match-p end-regex-or-time (process-get process 'output))) ;若获取到的值符合end-regex,则表示读取到了完整的output
-			 (accept-process-output process nil nil t)))
-		  ((numberp end-regex-or-time)
-		   (while (accept-process-output process end-regex-or-time nil t) ;若一段时间内无值,则认为已经读取了完整的output,退出循环等待
-			 )))
-	(setf output (process-get process 'output))
-	(process-put process 'output "")
-	(set-process-filter process originally-filter-function)
-	output))
+  (let (output)
+	(with-filter-function process (make-output-filter-function (process-filter process))
+						  (cond ((stringp end-regex-or-time)
+								 ;; 若获取到的值符合end-regex,则表示读取到了完整的output
+								 (while (not (string-match-p end-regex-or-time (process-get process 'output)))
+								   (accept-process-output process nil nil t)))
+								;; 若一段时间内无值,则认为已经读取了完整的output,退出循环等待
+								((numberp end-regex-or-time)
+								 (while (accept-process-output process end-regex-or-time nil t) 
+								   ))))
+						  (setf output (process-get process 'output))
+						  (process-put process 'output "")
+						  output))
 
 ;;;; 创建远程连接
-(defun make-connect-by-plink (remote usr pwd)
-  "通过plink建立与remote的远程连接"
-  (let* ((connect-name (format "%s@%s" usr remote))
-		 (connect-buffer connect-name))
-	(start-process connect-name connect-buffer "plink" "-l" usr "-pw" pwd remote)))
+  (defun make-connect-by-plink (remote usr pwd)
+	"通过plink建立与remote的远程连接"
+	(let* ((connect-name (format "%s@%s" usr remote))
+		   (connect-buffer connect-name))
+	  (start-process connect-name connect-buffer "plink" "-l" usr "-pw" pwd remote)))
 
-(defun make-connect-by-ssh (remote usr pwd)
-  "通过ssh建立与remote的远程连接"
-  (let* ((connect-name (format "%s@%s" usr remote))
-		 (connect-buffer connect-name)
-		 (process (start-process connect-name connect-buffer "ssh" "-l" usr remote)))
-	(accept-process-output process nil nil t)
-	(process-send-string process (concat pwd "\n") )
-	process))
+  (defun make-connect-by-ssh (remote usr pwd)
+	"通过ssh建立与remote的远程连接"
+	(let* ((connect-name (format "%s@%s" usr remote))
+		   (connect-buffer connect-name)
+		   (process (start-process connect-name connect-buffer "ssh" "-l" usr remote)))
+	  (accept-process-output process nil nil t)
+	  (process-send-string process (concat pwd "\n") )
+	  process))
 
-(defun make-connect (remote usr pwd)
-  (cond ((executable-find "plink")
-		 (make-connect-by-plink remote usr pwd))
-		((executable-find "ssh")
-		 (make-connect-by-ssh remote usr pwd))
-		(t (error "没找到建立远程连接的程序"))))
+  (defun make-connect (remote usr pwd)
+	(cond ((executable-find "plink")
+		   (make-connect-by-plink remote usr pwd))
+		  ((executable-find "ssh")
+		   (make-connect-by-ssh remote usr pwd))
+		  (t (error "没找到建立远程连接的程序"))))
 
-(defun make-or-raise-connect (remote usr pwd)
-  "若已经有usr@remote的连接,则直接返回该连接process,否则新建一个连接process"
-  (let ((connect-name (format "%s@%s" usr remote)))
-	(or (get-process connect-name)
-		(make-connect remote usr pwd))))
+  (defun make-or-raise-connect (remote usr pwd)
+	"若已经有usr@remote的连接,则直接返回该连接process,否则新建一个连接process"
+	(let ((connect-name (format "%s@%s" usr remote)))
+	  (or (get-process connect-name)
+		  (make-connect remote usr pwd))))
 
-(defun start-connect-process (remote usr &optional pwd wait-time)
-  "创建一个与远程服务器相连的连接process
+  (defun start-connect-process (remote usr &optional pwd wait-time)
+	"创建一个与远程服务器相连的连接process
 
 该函数返回连接到usr@remote的process,并且该process的end-output-regex记录了命令提示符的值,可以使用命令提示符来标识一个命令是否执行完毕"
-  (let* ((pwd (or pwd (read-passwd (format "请输入%s@%s的登录密码:" usr remote))))
-		 (wait-time (or wait-time 3))
-		 (process (make-or-raise-connect remote usr pwd)))
-	(process-put process 'output "")
-	(get-process-complete-output process wait-time) ;确定登录完成了
-	(cl-labels ((get-last-line (process)
-							   "获取process buffer中最后一行的内容"
-							   (with-current-buffer (process-buffer process) 
-								 (goto-char (point-max))
-								 (search-backward-regexp "[\r\n]")
-								 (buffer-substring-no-properties (1+ (point)) (point-max)))))
-	  (process-put process 'end-output-regex (regexp-quote (get-last-line process))))
-	process))
+	(let* ((pwd (or pwd (read-passwd (format "请输入%s@%s的登录密码:" usr remote))))
+		   (wait-time (or wait-time 3))
+		   (process (make-or-raise-connect remote usr pwd)))
+	  (process-put process 'output "")
+	  (get-process-complete-output process wait-time) ;确定登录完成了
+	  (cl-labels ((get-last-line (process)
+								 "获取process buffer中最后一行的内容"
+								 (with-current-buffer (process-buffer process) 
+								   (goto-char (point-max))
+								   (search-backward-regexp "[\r\n]")
+								   (buffer-substring-no-properties (1+ (point)) (point-max)))))
+		(process-put process 'end-output-regex (regexp-quote (get-last-line process))))
+	  process))
 
-(provide 'darksun-process-helper)
+  (provide 'darksun-process-helper)
 
 
