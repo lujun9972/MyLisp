2ad7b4c31a7de2ef2783d11e7a73928cc807416a
lujun9972 <lujw@drcbank.com>
2015-03-18 23:13:48 +0800

    不预设autoftp-local-remote-root-alist

--- autoftp.el
+++ autoftp.el
@@ -11,17 +11,14 @@
   (remove-hook 'after-save-hook 'UpMeToFtp))
 
 (defgroup autoftp-group nil "自动ftp到remote machine")
-(defcustom local-remote-root-pairs '(("e:/git-svn/server/" . "/cnaps2@10.8.6.10:")
-									 ("/cygdrive/e/git-svn/server/" . "/cnaps2@10.8.6.10:")
-									 ("~/trunk/" . "/cnaps2@10.8.6.10:")
-									 ("d:/workcvs/ibps/ibps" . "/ibpsusr@10.8.6.10:"))
+(defcustom autoftp-local-remote-root-alist nil 
   "本地项目root与对应的remote地址的alist
 
 其中remote-root的格式为/username@ip:/path/to/root/
 以local-root开头的文件会被ftp到对应的remote-root上去")
 
 
-(defun local-path-to-remote-path(local-path)
+(defun autoftp-local-path-to-remote-path(local-path)
   "转换本地路径为ftp的远程路径"
   (some (lambda (local-remote-root-pair)
 		  (let ((local-root (car local-remote-root-pair))
@@ -29,15 +26,14 @@
 			(if (string-prefix-p local-root local-path)
 				(replace-regexp-in-string (concat "^" (regexp-quote local-root)) remote-root local-path)
 			  nil)))
-		local-remote-root-pairs)) 
+		autoftp-local-remote-root-alist)) 
 
-;; (local-path-to-remote-path "e:/git-svn/server/trunk/makeall")
 (global-set-key [f11] 'UpMeToFtp)
 (defun UpMeToFtp()
   "Upload me to the ftp "
   (interactive)
   (let (remote-path)
-	(setq remote-path (local-path-to-remote-path buffer-file-name))
+	(setq remote-path (autoftp-local-path-to-remote-path buffer-file-name))
   (if remote-path
 	  (copy-file buffer-file-name  remote-path t))))  
 
