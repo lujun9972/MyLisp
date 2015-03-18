e6b57e093b344605c84e26e8940d2063f1e0ec3a
lujun9972 <lujw@drcbank.com>
2015-03-18 22:17:57 +0800

    增加从李杀网中摘录的使用外部程序打开文件的函数

--- darksun-file-helper.el
+++ darksun-file-helper.el
@@ -12,10 +12,10 @@
 	(insert-file-contents-literally path)
 	(md5 (buffer-string))))
 
+;; 以下函数摘自李杀网
 (defun fullpath-relative-to-current-file (file-relative-path)
   "Returns the full path of FILE-RELATIVE-PATH, relative to file location where this function is called.
 
-摘自李杀网
 Example: If you have this line
  (fullpath-relative-to-current-file \"../xyz.el\")
 in the file at
@@ -45,5 +45,41 @@ file's full path regardless the file is run by “load” or
 interactively by “eval-buffer”."
   (file-concat (or load-file-name buffer-file-name) file-relative-path))
 
+
+(defun ergoemacs-open-in-external-app ()
+  "使用外部程序打开dired-mode中被标记的文件"
+  (interactive)
+  (let ( doIt
+		 (myFileList
+		  (cond
+		   ((string-equal major-mode "dired-mode") (dired-get-marked-files))
+		   (t (list (buffer-file-name))) ) ) )
+    
+	(setq doIt (if (<= (length myFileList) 5)
+				   t
+				 (y-or-n-p "Open more than 5 files?") ) )
+    
+	(when doIt
+	  (cond
+	   ((string-equal system-type "windows-nt")
+		(mapc (lambda (fPath) (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t)) ) myFileList)
+		)
+	   ((string-equal system-type "darwin")
+		(mapc (lambda (fPath) (shell-command (format "open \"%s\"" fPath)) )  myFileList) )
+	   ((string-equal system-type "gnu/linux")
+		(mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath)) ) myFileList) ) ) ) ) )
+
+
+(defun ergoemacs-open-in-desktop ()
+  "使用系统的文件管理器打开文件"
+  (interactive)
+  (cond
+   ((string-equal system-type "windows-nt")
+	(w32-shell-execute "explore" (replace-regexp-in-string "/" "\\" default-directory t t)))
+   ((string-equal system-type "darwin") (shell-command "open ."))
+   ((string-equal system-type "gnu/linux")
+	(let ((process-connection-type nil)) (start-process "" nil "xdg-open" "."))
+	;; (shell-command "xdg-open .") ;; 2013-02-10 this sometimes froze emacs till the folder is closed. ⁖ with nautilus
+	) ))
 
 (provide 'darksun-file-helper)
