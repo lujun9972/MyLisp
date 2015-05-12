5079b8524e04ae4121acecf547ff7a824d8efd26
lujw <lujw@drcbank.com>
2015-05-12 09:50:52 +0800

    鏂扮殑UpMeToFtp鏀寔C-u淇敼tramp杩滅▼ip

--- autoftp.el
+++ autoftp.el
@@ -14,27 +14,36 @@
 (defcustom autoftp-local-remote-root-alist nil 
   "本地项目root与对应的remote地址的alist
 
-其中remote-root的格式为/username@ip:/path/to/root/
+其中remote-root的格式为username ip /path/to/root/
 以local-root开头的文件会被ftp到对应的remote-root上去")
 
+(defun autoftp--make-remote-path(username ip path-to-root &optional change-ip)
+  "拼出远程tramp路径"
+  (when change-ip
+	(setq ip (read-string "请输入远程ip:")))
+  (format "/%s@%s:%s" username ip path-to-root))
 
-(defun autoftp-local-path-to-remote-path(local-path)
+(defun autoftp--local-path-to-remote-path(local-path &optional change-ip)
   "转换本地路径为ftp的远程路径"
   (some (lambda (local-remote-root-pair)
-		  (let ((local-root (car local-remote-root-pair))
-				(remote-root (cdr local-remote-root-pair)))
+		  (let* ((local-root (car local-remote-root-pair))
+				 (remote-root-elements (cdr local-remote-root-pair))
+				 (username (first remote-root-elements))
+				 (ip (second remote-root-elements))
+				 (path-to-root (third remote-root-elements))
+				 (remote-root (autoftp--make-remote-path username ip path-to-root change-ip)))
 			(if (string-prefix-p local-root local-path)
 				(replace-regexp-in-string (concat "^" (regexp-quote local-root)) remote-root local-path)
 			  nil)))
 		autoftp-local-remote-root-alist)) 
 
 (global-set-key [f11] 'UpMeToFtp)
-(defun UpMeToFtp()
+(defun UpMeToFtp(&optional change-ip)
   "Upload me to the ftp "
-  (interactive)
+  (interactive "p")
   (let (remote-path)
-	(setq remote-path (autoftp-local-path-to-remote-path buffer-file-name))
-  (if remote-path
-	  (copy-file buffer-file-name  remote-path t))))  
+	(setq remote-path (autoftp--local-path-to-remote-path buffer-file-name change-ip))
+	(if remote-path
+		(copy-file buffer-file-name  remote-path t))))  
 
 (provide 'autoftp)
