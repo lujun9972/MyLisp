896a685cf3e649dad894ef66de1f8411cc35a308
darksun <lujun9972@sina.com>
2015-05-25 15:54:18 +0800

    elisp宏相关函数

---
+++ macro-helper.el
@@ -0,0 +1,11 @@
+;;; 宏相关工具
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro once-only ((&rest names) &body body)
+  (let ((gemsyms (loop for n in names collect (gensym))))
+    `(let (,@(loop for g in gensyms collect `(,g (gensym))))
+       `(let (,,@(loop for g in gensyms for n in names collect ``(,,g ,,n)))
+	  ,(let (,@(loop for n in names for g in gensyms collect `(,n ,g)))
+		,@body)))))
