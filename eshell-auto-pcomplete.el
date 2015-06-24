77c810644bf2018cfb79d972e944021473528a5a
darksun <lujun9972@sina.com>
2015-06-24 18:59:27 +0800

    pcomplete-completions中可能抛出'pcompleted

--- eshell-auto-pcomplete.el
+++ eshell-auto-pcomplete.el
@@ -3,18 +3,22 @@
 (defun ido-pcomplete ()
   "使用ido作为eshell的pcomplete方法"
   (interactive)
-  (let* (completion-result
-		 (completions (pcomplete-completions))
-		 (candidates (all-completions pcomplete-stub completions))
-		 (pcomplete-stub (replace-regexp-in-string ".*[\/]" "" pcomplete-stub))
-		 )
-	(cond ((null candidates)
-		   (error "没有匹配项"))
-		  ((= 1 (length candidates))
-		   (setq completion-result (car candidates)))
-		  (t (setq completion-result (ido-completing-read ": " candidates nil nil pcomplete-stub))))
-	(delete-char (- (length pcomplete-stub)))
-	(insert completion-result)))
+;; @ To simplify completion function logic, the tag `pcompleted' may
+;;   be thrown with a value of nil in order to abort the function.  It
+;;   means that there were no completions available.
+  (catch 'pcompleted
+	(let* (completion-result
+		   (completions (pcomplete-completions))
+		   (candidates (all-completions pcomplete-stub completions))
+		   (pcomplete-stub (replace-regexp-in-string ".*[\/]" "" pcomplete-stub))
+		   )
+	  (cond ((null candidates)
+			 (error "没有匹配项"))
+			((= 1 (length candidates))
+			 (setq completion-result (car candidates)))
+			(t (setq completion-result (ido-completing-read ": " candidates nil nil pcomplete-stub))))
+	  (delete-char (- (length pcomplete-stub)))
+	  (insert completion-result))))
 
 (when eshell-mode-map
   (define-key eshell-mode-map (kbd "<tab>") 'ido-pcomplete))
