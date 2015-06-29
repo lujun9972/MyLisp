0194e15d3cc5fc61ce66e0c024c0018932003f2c
lujun9972 <lujun9972@gmail.com>
2015-06-29 23:34:23 +0800

    增加函数根据配置文件生成地图

--- MUD/room-maker.el
+++ MUD/room-maker.el
@@ -22,9 +22,43 @@
 							   (split-string (file-content room-config-file) "[\r\n]"))))
 	(mapc #'build-room file-lines)))
 
+
+(defvar room-map nil
+  "room的地图")
+
 (defun build-room-map(room-map-config-file)
   "根据`room-map-config-file'中的配置信息创建地图"
-  )
+  (let* ((file-lines (split-string (file-content room-map-config-file) "[\r\n]")))
+	(setq room-map (mapcar (lambda(line)
+							  (mapcar #'intern (split-string line)))
+							file-lines))))
+
+(defun get-room-position (room-symbol room-map)
+  "从`room-map'中取出`room-symbol'标识的room的坐标"
+  (let* ((x (cl-position-if (lambda(x-rooms)
+							  (member room-symbol x-rooms)) room-map))
+		 (y (cl-position room-symbol (nth x room-map))))
+	(list x y)))
+
+(defun beyond-rooms (room-symbol room-map)
+  "根据room-map取与room-symbol相邻的room列表"
+  (cl-multiple-value-bind (x y) (get-room-position room-symbol room-map)
+	(let ((height (length room-map))
+		  (width (length (car room-map)))
+		  up down left right)
+	  (setq up (if (= x 0)
+				   nil
+				 (nth y (nth (1- x) room-map))))
+	  (setq down (if (= x (1- height))
+					 nil
+				   (nth y (nth (1+ x) room-map))))
+	  (setq left (if (= y 0)
+					 nil
+				   (nth (1- y) (nth x room-map))))
+	  (setq right (if (= y (1- width))
+					  nil
+					(nth (1+ y) (nth x room-map))))
+	  (list up right down left))))
 
 (provide 'room-maker)
 
