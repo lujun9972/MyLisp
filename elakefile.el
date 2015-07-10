192f921e73572942a4ee2681ad7ef5c955d78491
lujun9972 <lujun9972@gmail.com>
2015-07-10 21:03:26 +0800

    移动文件路径

--- elakefile.el
+++
@@ -1,9 +0,0 @@
-(elake-task say-hello (wash) 
-  "say hello to "
-  (message "hello"))
-(elake-task wash () 
-  "wash faces"
-  (message "wash"))
-
-(elake-task go-out (say-hello  wash)
-  (message "go out"))
