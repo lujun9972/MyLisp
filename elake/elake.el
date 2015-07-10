2a4447f4ab0038f4de03723d6c299db3e1da779a
darksun <lujun9972@sina.com>
2015-07-10 16:57:34 +0800

    增加-p选项显示任务的预备条件

--- elake/elake.el
+++ elake/elake.el
@@ -6,6 +6,7 @@
   (defvar elake-executed-task nil
 	"已经执行过的task,不要重新执行"))
 
+;; 定义task
 (defmacro elake-task (task-symbol prepare-task-list &optional doc-string &rest body)
   "使用elask-task宏来定义task"
   (declare (indent defun))
@@ -17,6 +18,7 @@
 	 ,@body)
   )
 
+;; 执行task函数
 (defun elake--execute-task (task-symbol)
   "运行`task-symbol'标识的任务,会预先运行它的prepare-tasks"
   (when (stringp task-symbol)
@@ -37,6 +39,7 @@
 (defun elake-execute-task ()
   (elake--execute-task argi))
 
+;; 显示任务说明
 (defun elake--task-documentation (&rest task-symbols)
   "显示`task-symbol'指定任务的说明"
   (when (null task-symbols)
@@ -51,8 +54,26 @@
   (apply 'elake--task-documentation command-line-args-left)
   (setq command-line-args-left nil))
 
+;; 显示task的准备条件
+(defun elake--task-preparation (&rest task-symbols)
+  "显示`task-symbol'指定任务的说明"
+  (when (null task-symbols)
+	(require 'subr-x)
+	(setq task-symbols (hash-table-keys elake-task-relationship)))
+  (mapc (lambda (task-symbol)
+		  (when (stringp task-symbol)
+			(setq task-symbol (intern task-symbol)))
+		  (message "%s:%s" task-symbol (gethash task-symbol elake-task-relationship))) task-symbols))
+
+(defun elake-task-preparation (option)
+  (apply 'elake--task-preparation command-line-args-left)
+  (setq command-line-args-left nil))
+
+;; 加载elakefile文件
 (add-to-list 'load-path (file-name-directory load-file-name))
 (load "elakefile" nil t)
 
+;; 设置参数处理函数
 (add-to-list 'command-switch-alist '("--task" . elake-task-documentation))
+(add-to-list 'command-switch-alist '("-p" . elake-task-preparation))
 (add-to-list 'command-line-functions 'elake-execute-task)
