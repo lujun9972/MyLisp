9a8110c758330fb28be1f0a2c84612caf959c3de
lujun9972 <lujun9972@gmail.com>
2015-07-20 22:03:59 +0800

    修改处理html时出现嵌套同tag时会跳过内部tag的问题

--- url-helper.el
+++ url-helper.el
@@ -1,16 +1,52 @@
+(defun url-get-content-from-html-async (url)
+  ""
+	(url-retrieve url (lambda (status)
+						(setq url-content (libxml-parse-xml-region (point-min) (point-max))))))
+
+(defun url-get-content-from-html(url)
+  "从http `url'中获取经过`libxml-parse-html-region'解析的内容"
+  (let ((url-buffer (url-retrieve-synchronously url))
+		url-content)
+	(with-current-buffer url-buffer
+	  (setq url-content (libxml-parse-html-region (point-min) (point-max))))
+	(kill-buffer url-buffer)
+	url-content))
+(defun url-get-thing-from-html(url tag &optional attr regexp)
+  "从http `url'中tag标签下,attr属性且获取匹配`regexp'的值. 其中attr表示属性时,格式为":属性"
+
+`url'可以是一个代表url的字符串,或者url-content. `attr'默认为'text,表示tag标签保住的内容,`regexp'默认为空字符串,表示匹配值"
+  (unless attr
+	(setq attr 'text))
+  (unless regexp
+	(setq regexp ""))
+  (let ((url-content (if (stringp url)
+						 (url-get-content-from-html url)
+					   url))
+		url-things)
+	(with-temp-buffer 
+	  (flet ((save-url-thing-fn (cont)
+								(dolist (sub cont)
+								  (cond ((eq (car sub) attr)
+										 (when (string-match-p regexp (cdr sub))
+										   (push (cdr sub) url-things)))
+										((listp (cdr sub))
+										 (let ((things (save-url-thing-fn (cdr sub))))
+										   (when things
+											 (push things url-things)))))))) ;处理嵌套tag
+		(let ((shr-external-rendering-functions `((,tag . save-url-thing-fn))))
+		  (shr-insert-document url-content))))
+	url-things))
+
 (defun url-get-title-from-html(url)
   "从http url中获取title"
-  (let ((url-buffer (url-retrieve-synchronously url))
-		url-content
-		url-title)
-	(with-current-buffer url-buffer
-	  (setq url-content (libxml-parse-html-region (point-min) (point-max)))
-	  (flet ((save-url-title-fn (cont)
-								(dolist (sub cont)
-								  (when (eq (car sub) 'text)
-									(setq url-title (cdr sub))))))
-		(let ((shr-external-rendering-functions '((title . save-url-title-fn))))
-		  (shr-insert-document url-content))))
-	(kill-buffer url-buffer)
-	url-title))
+  (url-get-thing-from-html url 'title))
+
+(defun url-get-links-from-html(url &optional regexp)
+  "从http `url'中获取匹配`regexp'的超链接地址"
+  (url-get-thing-from-html url 'a :href regexp))
+
+(defun url-get-images-from-html(url &optional regexp)
+  "从http `url'中获取匹配`regexp'的图片地址"
+  (url-get-thing-from-html url 'img :src regexp))
+
 (provide 'url-helper)
