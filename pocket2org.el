341ba9f2853efefef6fd911e1d361b2824b277fb
darksun <lujun9972@gmail.com>
2017-02-14 15:21:52 +0800

    update

--- pocket2org.el
+++ pocket2org.el
@@ -3,14 +3,14 @@
   (let* ((response (pocket-api-get :offset offset :count count))
          (records (cdr (assoc-string "list" response)))
          (urls (mapcar (lambda (record)
-                         (cdr (assoc-string 'given_url record)))
+                         (cdr (assoc-string 'resolved_url record)))
                        records)))
     (when urls
       (mapcar callback  urls)
       (pocket2org (+ offset count) count callback))))
 
 (let ((url2org-store-dir "/tmp")
-      (url2org-max-process 5))
+      (url2org-max-process 1))
   (pocket2org 1 1000 (lambda (url)
-                       (ignore-errors (url2org url))) ))
-
+                       (ignore-errors (url2org url)))))
+;; http://paulgraham.com/avg.html
--- url2org.el
+++ url2org.el
@@ -19,34 +19,46 @@
 (defvar url2org-process-num 0
   "Number of w3m process")
 
-(defun url2org--save-to-org (&rest ignore)
-  (message "storing %s" w3m-current-url)
-  (setq url2org-process-num (- url2org-process-num 1))
+(defvar url2org-urls nil)
+
+(defcustom url2org-timeout 30
+  "Timeout seconds"
+  :type 'number)
+
+(defun url2org--save-to-org (w3m-buf)
+  (with-current-buffer w3m-buf
+    (message "storing %s" w3m-current-url)
+    (setq url2org-urls (delete w3m-current-title url2org-urls))
+    (setq url2org-process-num (max 0 (- url2org-process-num 1)))
+    (url2org-save)
+    (when url2org-auto-kill-p
+      (kill-buffer))))
+
+(defun url2org-save ()
+  (interactive)
   (let* ((title (w3m-current-title))
          (filename (concat (file-name-as-directory url2org-store-dir) title ".org"))
          (content (progn
                     (org-w3m-copy-for-org-mode)
                     (with-temp-buffer
                       (yank)
-                      (goto-char (point-min))
-                      (search-forward "location")
-                      (replace-match "#+URL")
                       (buffer-string)))))
     (with-temp-file filename
-      (insert content))
-    (when url2org-auto-kill-p
-      (kill-buffer))))
+      (insert "#+URL: " w3m-current-url)
+      (newline)
+      (insert content))))
 
 (defun url2org (url)
   (interactive "surl:")
+  (push url url2org-urls)
   (while (>= url2org-process-num url2org-max-process)
     (sit-for 1))
   (setq url2org-process-num (+ url2org-process-num 1))
   (w3m-goto-url-new-session url)
-  (set (make-local-variable 'w3m-display-hook) (cons #'url2org--save-to-org w3m-display-hook)))
-
-;; (mapc (lambda (url)
-;;         (ignore-errors
-;;           (url2org url)))  '("https://blogs.msdn.microsoft.com/typescript/2015/11/03/what-about-asyncawait/" "https://utcc.utoronto.ca/~cks/space/blog/programming/CodeEditingVimVsEmacs"))
-
-;; (mapc 'url2org '("http://www.baidu.com" "http://www.bing.com"))
+  (let ((buf (current-buffer)))
+    (run-with-timer url2org-timeout nil (lambda ()
+                                          (when (buffer-live-p buf)
+                                            (setq url2org-process-num (- url2org-process-num 1))
+                                            (kill-buffer buf))))
+    (set (make-local-variable 'w3m-display-hook) (lambda (&rest ignore)
+                                                   (url2org--save-to-org buf)))))
