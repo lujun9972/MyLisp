60c2efb8508e206454e3fc3d7e3c9fd435f36273
darksun <lujun9972@gmail.com>
2017-02-15 21:42:36 +0800

    update

--- pocket2org.el
+++ pocket2org.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; -*-
+
 (require 'cl-lib)
 (cl-defun pocket2org (&optional (offset 1) (count 10) (recur t))
   (let* ((response (pocket-api-get :offset offset :count count))
@@ -5,12 +7,15 @@
          (urls (mapcar (lambda (record)
                          (cdr (assoc-string 'resolved_url record)))
                        records)))
+    (when urls
+      (mapcar (lambda (url)
+                (ignore-errors
+                  (url2org url))) urls) )
     (when (and urls recur)
-      (mapcar #'url2org  urls)
       (pocket2org (+ offset count) count callback))))
 
-(let ((url2org-store-dir "~/pocket")
-      (url2org-max-process 2))
-  (pocket2org 1500 1000))
+(let ((url2org-store-dir "/tmp")
+      (url2org-max-process 3))
+  (pocket2org 1800 1000 nil))
 
 ;; http://paulgraham.com/avg.html
